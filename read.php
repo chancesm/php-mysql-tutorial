65ca01b79748d9ec9f537226a9daf34b08cfb12d
Chance Murray <chance.s.murray@gmail.com>
2019-09-30 11:20:32 -0600

    Update read.php

--- read.php
+++ read.php
@@ -1,6 +1,6 @@
 <?php
 include 'settings.php';
-$conn = dbConnect();
+$conn = new mysqli($dbaddress, $dbuser, $dbpass, $dbname);
 if ($result = $conn->query("Select * from studentform;")) {
   $data = $result->fetch_all(MYSQLI_ASSOC);
   var_dump($data);
